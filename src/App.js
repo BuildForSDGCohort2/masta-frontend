5ad5972f20d07e185b45f56b461c5692bb943c39
Phil Odinga <phil.odinga@astechcongo.com>
2020-10-12 16:31:07 -0500

    alan update

--- src/App.js
+++ src/App.js
@@ -8,18 +8,48 @@ import Hospital from "./App/components/Hospital";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
+  // useEffect(() => {
+  //   alanBtn({
+  //     className: "chat-plus-btn",
+  //     key:"0ea827ac2712d63fc6ba9fadc5a62e202e956eca572e1d8b807a3e2338fdd0dc/prod",
+  //     onCommand: (commandData) => {
+  //       if (commandData.command === "go:back") {
+  //       }
+  //     },
+  //     onConnectionStatus: async function (status) {
+  //       if (status === 'authorized') {
+  //         await alanBtnInstance.activate();
+  //         alanBtnInstance.playText('WELCOME TO MASTA HEALTHCARE');
+  //       }
+  //     },
+  //     position: "fixed",
+  //     bottom: "25px",
+  //     right: "40px",
+  //     padding: "10px",
+      
+  //   });
+  // }, []);
+
   useEffect(() => {
-    alanBtn({
-      className: "chat-plus-btn",
-      key:
-        "0ea827ac2712d63fc6ba9fadc5a62e202e956eca572e1d8b807a3e2338fdd0dc/prod",
+    let alanBtnInstance = alanBtn({
+      key:"0ea827ac2712d63fc6ba9fadc5a62e202e956eca572e1d8b807a3e2338fdd0dc/prod",
       onCommand: (commandData) => {
-        if (commandData.command === "go:back") {
+        if (commandData.command === 'go:back') {
+          // Call the client code that will react to the received command
         }
       },
+      onConnectionStatus: async function (status) {
+        if (status === 'authorized') {
+          await alanBtnInstance.activate();
+          alanBtnInstance.playText('Good morning Phil Odinga , I\'m Philia, your personal medical assistant. what can I do for you today?');
+        }
+      },
+      position: "fixed",
+      bottom: "25px",
+      right: "40px",
+      padding: "10px",
     });
   }, []);
-
   return (
       <div id="wrapper">
         <_sidebar></_sidebar>
